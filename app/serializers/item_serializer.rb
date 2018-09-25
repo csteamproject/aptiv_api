cd49b099a7e664d18dffef8f561088d37abb3e5e
Robert Dale <rdale28@gmail.com>
2018-09-25 00:41:39 -0600

    Adds working models for items and computers.

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,3 +1,4 @@
 class ItemSerializer < ActiveModel::Serializer
   attributes :id, :name, :price, :quantity
+  has_one :computer, serializer: ComputerSerializer
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_24_224626) do
+ActiveRecord::Schema.define(version: 2018_09_24_225847) do
+
+  create_table "computers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "utag"
+    t.decimal "cpu", precision: 5, scale: 2
+    t.integer "ram"
+    t.integer "hdd"
+    t.bigint "item_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_computers_on_item_id"
+  end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
