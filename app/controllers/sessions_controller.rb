4bda56a2c7a5d005a1d56e4b5c368346f5156061
Robert Dale <rdale28@gmail.com>
2018-10-24 02:10:10 -0600

    Adds user roles, fixes issues with roles in seeded data, modifies return header for successful login with first, last, and role_id

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     @user = User.find_by(username: @username).try(:authenticate, @password)
     if @user
       token = JWT.encode({user_id: @user.id, authentication_date: Time.now}, Rails.application.credentials.secret_key_base)
-      render json: {success: true, token: token}
+      render json: {success: true, token: token, first: @user.first_name, last: @user.last_name, role: @user.role_id}
     else
       render json: {success: false}
     end
