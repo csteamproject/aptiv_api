45025f0493adf019ced3b100f6a6abf3b0028147
Robert Dale <rdale28@gmail.com>
2018-10-24 02:25:53 -0600

    Modifies role_id session return as a string and not an integer.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
   has_secure_password
   validates :username, presence: true
   belongs_to :role
+
+  scope :role_name, -> { User.find_by(:id).includes(:role) }
+
 end
