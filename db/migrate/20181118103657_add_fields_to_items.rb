5115a74a1c74f9a5df5d2a266a919b10ae79c3fe
Robert Dale <rdale28@gmail.com>
2018-11-20 16:17:39 -0700

    fixes issue with integer and biginteger, adds return json string as file uploaded.

--- db/migrate/20181118103657_add_fields_to_items.rb
+++ db/migrate/20181118103657_add_fields_to_items.rb
@@ -4,7 +4,7 @@ class AddFieldsToItems < ActiveRecord::Migration[5.2]
     add_column :items, :brand, :string
     add_column :items, :model, :string
     add_column :items, :operable, :boolean
-    add_column :items, :checkout_user_id, :integer
+    add_column :items, :checkout_user_id, :bigint
     add_foreign_key :items, :users, column: :checkout_user_id, primary_key: :id
   end
 end
